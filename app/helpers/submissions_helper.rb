526b7c7d846c07bc692cf31a961bfe6fc455c0be
Stephen Lind <stephen.l@getjobber.com>
2018-05-15 13:43:39 -0600

    fixed cutoff dates and showing them in submissions

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -5,7 +5,7 @@ module SubmissionsHelper
     controls = []
     controls << (link_to 'download PDF', submission_download_path(submission, format: 'pdf'), target: '_blank', class: 'download')
     if user.is_admin? || (submission.user == user && !submission.submitted)
-      controls << (link_to 'edit', edit_submission_path(submission))
+      controls << (link_to 'edit', edit_submission_path(submission)) if submission.cohort.edit_submission_cutoff_date > Time.now
       controls << (link_to "delete", submission, method: :delete, data: { confirm: "You sure?" })
     end
     controls.join(" | ")
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -5,4 +5,12 @@ class Cohort < ApplicationRecord
   accepts_nested_attributes_for :categories, reject_if: :all_blank, allow_destroy: true
 
   validates :steel_work_completed_deadline, presence: true
+  validates :new_submission_cutoff_date, presence: true
+  validates :edit_submission_cutoff_date, presence: true
+
+  scope :active, -> { where(active: true).first }
+
+  def new_cutoff_date_invalid?
+    new_submission_cutoff_date < Time.now
+  end
 end
