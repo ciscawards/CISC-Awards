89eb16f3bcb0a1236384d590fa8e1bb6d2999814
Stephen Lind <stephen.l@getjobber.com>
2018-06-23 14:52:33 -0600

    update production host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'stark-basin-78087.herokuapp.com/'
+  host = 'cisc-alberta-steel-design-awards.network/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
