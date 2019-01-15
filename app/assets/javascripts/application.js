e97751e05b15e32b870ff06a518ed4ee9e82e426
Stephen Lind <stephen.l@getjobber.com>
2019-01-15 13:58:38 -0700

    editor looks nice!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,20 +25,20 @@
 //= require_tree .
 
 $.fraola_creds = {
-    toolbarStickyOffset: 50,
-        key: 'RH4I3B17A10iB6E5E4H5I2I3C7B6D6E5C-11bkpgC9gkjlxD-8mH4A-21D-17utE-11E1ppeB-21D-16wC2zkA-8lF-7sC5lfpriuc=='
+  toolbarStickyOffset: 50,
+  key: 'RH4I3B17A10iB6E5E4H5I2I3C7B6D6E5C-11bkpgC9gkjlxD-8mH4A-21D-17utE-11E1ppeB-21D-16wC2zkA-8lF-7sC5lfpriuc=='
 };
 
 $(document).ready(function(){
-    $('textarea:not(.no-wysiwyg)').froalaEditor($.fraola_creds);
-    $('.js_select_all').change(function() {
-        var checkboxes = $(this).closest('table').find(':checkbox');
-        checkboxes.prop('checked', $(this).is(':checked'));
-    });
+  $('textarea:not(.no-wysiwyg)').froalaEditor($.fraola_creds);
+  $('.js_select_all').change(function() {
+    var checkboxes = $(this).closest('table').find(':checkbox');
+    checkboxes.prop('checked', $(this).is(':checked'));
+  });
 });
 
 $(document).on('cocoon:after-insert', function(e, insertedItem) {
-    insertedItem.find('textarea').froalaEditor($.fraola_creds);
+  insertedItem.find('textarea').froalaEditor($.fraola_creds);
 });
 
 $.fn.datepicker.defaults.format = "yyyy-mm-dd";
