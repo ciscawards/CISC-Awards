08cb7d74173e4edfb837a5af79e99b10a61e5581
Stephen Lind <stephen.l@getjobber.com>
2019-01-15 12:01:34 -0700

    clean up duping

--- app/assets/javascripts/rails.validations.actionView.js
+++ app/assets/javascripts/rails.validations.actionView.js
@@ -10,10 +10,13 @@ window.ClientSideValidations.formBuilders['ActionView::Helpers::FormBuilder'] = 
         element.attr('autofocus', false);
       }
 
+      element.before(inputErrorField);
       if (element.is('textarea')){
-        element = element.parent().children('.fr-box, textarea')
+        inputErrorField.find('span#input_tag').replaceWith(element.parent().children('.fr-box, textarea'));
+      } else {
+        inputErrorField.find('span#input_tag').replaceWith(element);
       }
-      inputErrorField.find('span#input_tag').replaceWith(element);
+
       inputErrorField.find('label.message').attr('for', element.attr('id'));
       labelErrorField.find('label.message').attr('for', element.attr('id'));
       labelErrorField.insertAfter(label);
@@ -22,24 +25,24 @@ window.ClientSideValidations.formBuilders['ActionView::Helpers::FormBuilder'] = 
     return form.find("label.message[for='" + (element.attr('id')) + "']").text(message);
   },
   remove: function(element, settings) {
-    var errorFieldClass, form, inputErrorField, label, labelErrorField;
-    // Whole Form
-    form = $(element[0].form);
-    // "field_with_errors"
-    errorFieldClass = $(settings.label_tag).attr('class');
-    // This is the full input element, text_area is nested within
-    inputErrorField = element.closest("." + (errorFieldClass.replace(/\ /g, ".")));
-    // This is just the label, no error message with this
-    label = form.find("label[for='" + (element.attr('id')) + "']:not(.message)");
-    // This is the whole error field
-    labelErrorField = label.closest("." + errorFieldClass);
-    if (inputErrorField[0]) {
-      // This removes the text_area element
-      inputErrorField.find("#" + (element.attr('id'))).detach();
-      // TODO: This is where things are borking
-      inputErrorField.replaceWith(element);
-      label.detach();
-      return labelErrorField.replaceWith(label);
-    }
+    // var errorFieldClass, form, inputErrorField, label, labelErrorField;
+    // // Whole Form
+    // form = $(element[0].form);
+    // // "field_with_errors"
+    // errorFieldClass = $(settings.label_tag).attr('class');
+    // // This is the full input element, text_area is nested within
+    // inputErrorField = element.closest("." + (errorFieldClass.replace(/\ /g, ".")));
+    // // This is just the label, no error message with this
+    // label = form.find("label[for='" + (element.attr('id')) + "']:not(.message)");
+    // // This is the whole error field
+    // labelErrorField = label.closest("." + errorFieldClass);
+    // if (inputErrorField[0]) {
+    //   // This removes the text_area element
+    //   inputErrorField.find("#" + (element.attr('id'))).detach();
+    //   // TODO: This is where things are borking
+    //   inputErrorField.replaceWith(element);
+    //   label.detach();
+    //   return labelErrorField.replaceWith(label);
+    // }
   }
 };
